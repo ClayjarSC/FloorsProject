9a2f4fd3269d6116d98d18254ebcb12c7566c502
Alex Johnson <me@alex-johnson.net>
2016-02-16 15:37:52 -0500

    got a basic Unit abstract class done

--- src/modules/Unit.java
+++ src/modules/Unit.java
@@ -1,5 +1,37 @@
 package modules;
 
 public abstract class Unit {
-	
+    private int health;
+    private AttackMode attackMode;
+    
+    protected static enum AttackMode {
+        FRIENDLY, OFFENSIVE, DEFENSIVE, BOSS
+    }
+    
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public Unit(AttackMode mode) {
+        this.attackMode = mode;
+        health = 5;
+    }
+    
+    public AttackMode getAttackMode() {
+        return attackMode;
+    }
+    
+    public void setAttackMode(AttackMode mode) {
+        this.attackMode = mode;
+    }
+    
+    // Abstract methods that need to be implemented    
+    public abstract void action();    
+    public abstract void move();
+    public abstract void attack();
+    
 }
