ceaf02b2f56919c6008069e45e07b9af11bddee9
Alex Johnson <me@alex-johnson.net>
2016-02-29 20:27:11 -0500

    added direction and the other changes we talked about last week to Unit class

--- src/modules/Unit.java
+++ src/modules/Unit.java
@@ -3,9 +3,21 @@ package modules;
 public abstract class Unit {
     private int health;
     private AttackMode attackMode;
+    private Direction orientation;
+    private Status status;
+    private int moveSpeed;
     
     protected static enum AttackMode {
         FRIENDLY, OFFENSIVE, DEFENSIVE, BOSS
+    }
+    
+    protected static enum Status {
+        ACTIVE, DOWN, DEAD
+    }
+
+    public Unit(AttackMode mode) {
+        this.attackMode = mode;
+        health = 5;
     }
     
     public int getHealth() {
@@ -15,11 +27,6 @@ public abstract class Unit {
     public void setHealth(int health) {
         this.health = health;
     }
-
-    public Unit(AttackMode mode) {
-        this.attackMode = mode;
-        health = 5;
-    }
     
     public AttackMode getAttackMode() {
         return attackMode;
@@ -27,6 +34,30 @@ public abstract class Unit {
     
     public void setAttackMode(AttackMode mode) {
         this.attackMode = mode;
+    }
+    
+    public Direction getOrientation() {
+        return orientation;
+    }
+    
+    public void setOrientation(Direction orientation) {
+        this.orientation = orientation;
+    }
+    
+    public Status getStatus() {
+        return status;
+    }
+    
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+    
+    public int getSpeed() {
+        return moveSpeed;
+    }
+    
+    public void setSpeed(int moveSpeed) {
+        this.moveSpeed = moveSpeed;
     }
     
     // Abstract methods that need to be implemented
